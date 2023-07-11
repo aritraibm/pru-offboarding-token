e27e79c435aee5fec9d282df0c8b36e365aa945f
Manideep432 <Manideep.siripalli@ibm.com>
2023-07-11 11:14:07 +0530

    token changes

--- src/main/java/com/pru/token/app/jwt/JwtTokenUtil.java
+++ src/main/java/com/pru/token/app/jwt/JwtTokenUtil.java
@@ -1,21 +1,17 @@
 package com.pru.token.app.jwt;
 
-import java.util.Date;
-
+import com.pru.token.app.user.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.pru.token.app.user.User;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtTokenUtil {
@@ -38,33 +34,37 @@ public class JwtTokenUtil {
 				.compact();
 	}
 	
-	public boolean validateAccessToken(String token) {
-		try {
-			Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
-			return true;
-		} catch (ExpiredJwtException ex) {
-			LOGGER.error("JWT expired", ex.getMessage());
-		} catch (IllegalArgumentException ex) {
-			LOGGER.error("Token is null, empty or only whitespace", ex.getMessage());
-		} catch (MalformedJwtException ex) {
-			LOGGER.error("JWT is invalid", ex);
-		} catch (UnsupportedJwtException ex) {
-			LOGGER.error("JWT is not supported", ex);
-		} catch (SignatureException ex) {
-			LOGGER.error("Signature validation failed");
-		}
-		
-		return false;
-	}
-	
-	public String getSubject(String token) {
-		return parseClaims(token).getSubject();
-	}
-	
-	public Claims parseClaims(String token) {
-		return Jwts.parser()
-				.setSigningKey(SECRET_KEY)
-				.parseClaimsJws(token)
-				.getBody();
+//	public boolean validateAccessToken(String token) {
+//		try {
+//			Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+//			return true;
+//		} catch (ExpiredJwtException ex) {
+//			LOGGER.error("JWT expired", ex.getMessage());
+//		} catch (IllegalArgumentException ex) {
+//			LOGGER.error("Token is null, empty or only whitespace", ex.getMessage());
+//		} catch (MalformedJwtException ex) {
+//			LOGGER.error("JWT is invalid", ex);
+//		} catch (UnsupportedJwtException ex) {
+//			LOGGER.error("JWT is not supported", ex);
+//		} catch (SignatureException ex) {
+//			LOGGER.error("Signature validation failed");
+//		}
+//		
+//		return false;
+//	}
+//	
+//	public String getSubject(String token) {
+//		return parseClaims(token).getSubject();
+//	}
+//	
+//	public Claims parseClaims(String token) {
+//		return Jwts.parser()
+//				.setSigningKey(SECRET_KEY)
+//				.parseClaimsJws(token)
+//				.getBody();
+//	}
+	public Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
 	}
 }
